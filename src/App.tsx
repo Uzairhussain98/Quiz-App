9a272766146f21a91c7ffefce5b66123c452b85d
Uzair Hussain <uzairhussain98@hotmail.com>
2020-12-07 17:31:42 +0500

    linked API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react';
+import Questioncard from './components/Questioncard'
+import {fetchQuestions ,Difficulty} from './API'
+
+const TOTAL_QUESTIONS = 10 ;
 
 function App() {
+
+  const [loading , setLoading ] = useState(false);
+  const [questions,setQuestions ] = useState([]);
+  const [number , setNumber ] = useState(0);
+  const [userAnswer , setuserAnswer ] = useState([]);
+  const [ score , setScore ] = useState(0);
+  const [ gameover , setGameover ] = useState(true);
+
+  console.log(fetchQuestions(TOTAL_QUESTIONS , Difficulty.EASY ));
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {} ;
+
+
+  const Startquiz = async() => {}
+  const Next = async() => {}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Quiz App</h1>
+
+  <button className="start__quiz" onClick={Startquiz}>Begin Quiz</button>
+    <h3>Score :</h3>
+
+    <p>
+      Loading
+    </p>
+    {/* <Questioncard
+    questionNum ={number +1 }
+    totalQuestion = {TOTAL_QUESTIONS}
+    question = {questions[number].question}
+    answers = {questions[number].answers}
+    userAnswer = {userAnswer ? userAnswer[number] : undefined}
+    callback= {checkAnswer}
+
+    /> */}
+
+    <button className="next__q" onClick={Next} >Next</button>
+
+
     </div>
   );
 }
---
+++ src/components/Questioncard.tsx
@@ -0,0 +1,37 @@
+import { type } from 'os'
+import React from 'react'
+
+type Props = {
+    question :string;
+    answers :string[];
+    callback: any;
+    userAnswer:any;
+    questionNum: number;
+    totalQuestion : number;
+
+}
+
+
+const Questioncard : React.FC<Props> = ({question ,answers ,callback, userAnswer ,questionNum ,totalQuestion}) => {
+  return (
+    <div>
+       <p> question : {questionNum}/{totalQuestion}</p>
+       <p dangerouslySetInnerHTML={{__html: question}} />
+    <div>
+        {answers.map(answer =>
+        <div>
+            <button disabled={userAnswer} onClick={callback}> 
+            <span dangerouslySetInnerHTML={{__html: answer}} />
+            </button>
+        </div> 
+            )}
+
+
+    </div>
+
+
+    </div>
+  )
+}
+
+export default Questioncard
